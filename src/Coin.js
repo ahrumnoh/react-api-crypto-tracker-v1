da64de6b85546631261d01ac33ca53f302080209
ahrum noh <sydneyroseenglish@gmail.com>
2021-11-01 00:20:10 +1100

    updated
    push

---
+++ src/Coin.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Coin.css';
+
+const Coin = ({name, image, symbol, price, volume, priceChange, marketcap, lastUpdate}) => {
+    return (
+        <div className="coin-contatiner">
+            <div className="coin-row">
+                <div className="coin">
+                    <img src={image} alt="crypto"/>
+                    <h1>{name}</h1>
+                    <p className="coin-symbol">{symbol}</p>
+                </div>
+                <div className='coin-data'>
+                    <p className="coin-price">${price}</p>
+                    <p className="coin-volume">${volume.toLocaleString()}</p>
+                    {priceChange < 0 ? (
+                        <p className="coin-percent red">{priceChange.toFixed(2)}%</p> 
+                    ) : (
+                        <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+                        
+                    )}
+
+
+                    <p className="coin-marketcap">
+                        Mkt Cap: ${marketcap.toLocaleString()}
+                    </p>
+                    <p className="last_updated">{lastUpdate}</p>
+                    
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Coin;
